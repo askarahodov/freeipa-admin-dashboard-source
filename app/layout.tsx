d37f71500f3d7db9777312711cc4fb78307993f1
askarahodov <devops.bel@yandex.ru>
2026-07-24 11:16:19 +0300

    Load enhanced local auth styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./local-auth.css";
+import "./local-auth-enhancements.css";
 import LocalAuthToolbar from "./LocalAuthToolbar";
 
 const geistSans = Geist({

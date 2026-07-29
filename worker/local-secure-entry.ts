bc77720ef2c7db258a590854323f5560521f7ba6
askarahodov <devops.bel@yandex.ru>
2026-07-29 17:15:38 +0300

    Route settings through source override wrapper

--- worker/local-secure-entry.ts
+++ worker/local-secure-entry.ts
@@ -1,4 +1,4 @@
-import secureRuntime from "./settings-lifecycle-entry";
+import secureRuntime from "./settings-source-entry";
 import { appendAuditEvent, createAuditContext } from "../audit-log";
 import {
   isAdminIntegrationPath,

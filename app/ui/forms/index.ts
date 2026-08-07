13182e8c7b9ed56432066301da086823fbf79da5
askarahodov <devops.bel@yandex.ru>
2026-08-07 13:22:39 +0300

    refactor(ui): replay forms entrypoint

---
+++ app/ui/forms/index.ts
@@ -0,0 +1,5 @@
+export { Dialog, type DialogProps, type DialogSize } from "./Dialog";
+export { DialogFooter, type DialogFooterProps } from "./DialogFooter";
+export { FormErrorSummary, type FormErrorItem, type FormErrorSummaryProps } from "./FormErrorSummary";
+export { FormField, type FormFieldProps } from "./FormField";
+export { FormSection, type FormSectionProps } from "./FormSection";

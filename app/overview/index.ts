e996ea415a0af21d7ab75e380cb79d9850b5c17b
askarahodov <devops.bel@yandex.ru>
2026-08-07 12:11:23 +0300

    feat: export operational Overview module

---
+++ app/overview/index.ts
@@ -0,0 +1,13 @@
+export { OperationalOverview, type OperationalOverviewProps, type OverviewOperation, type OverviewQuickAction } from "./OperationalOverview";
+export {
+  buildOperationalOverview,
+  type OperationalOverviewInput,
+  type OperationalOverviewModel,
+  type OverviewAttentionItem,
+  type OverviewHealthRow,
+  type OverviewState,
+  type OverviewTarget,
+  type PortalHealthCheck,
+  type PortalHealthCheckName,
+  type PortalReadiness,
+} from "./operational-overview-model";

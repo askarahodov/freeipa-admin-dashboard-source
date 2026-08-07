c0e600269b34ebae69af4b5f36b7282d8292c898
askarahodov <devops.bel@yandex.ru>
2026-08-07 17:44:47 +0300

    UI architecture: extract home presentation contract (#177)
    * UI architecture: extract home presentation helpers
    
    * UI architecture: test home presentation contract
    
    * UI architecture: export home presentation helpers

--- app/shell/index.ts
+++ app/shell/index.ts
@@ -1,6 +1,12 @@
 export { AppShell, type AppShellProps } from "./AppShell";
 export { ProductIcon, type ProductIconProps } from "./ProductIcon";
 export {
+  buildAutomationSlug,
+  PROCESS_ICON_GLYPHS,
+  resolveProcessIconGlyph,
+  type ProcessKind,
+} from "./home-presentation";
+export {
   PRODUCT_NAV_GROUPS,
   isProductNavItemActive,
   normalizeProductPath,

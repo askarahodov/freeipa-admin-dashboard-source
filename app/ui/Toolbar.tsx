1501640399320113392d00f460db5aa2ba57a0ae
askarahodov <devops.bel@yandex.ru>
2026-08-07 11:36:47 +0300

    feat: add shared Toolbar primitive

---
+++ app/ui/Toolbar.tsx
@@ -0,0 +1,9 @@
+import type { HTMLAttributes } from "react";
+import styles from "./ui.module.css";
+
+export type ToolbarProps = HTMLAttributes<HTMLDivElement>;
+
+export function Toolbar({ className, ...props }: ToolbarProps) {
+  const classes = [styles.toolbar, className].filter(Boolean).join(" ");
+  return <div className={classes} role="toolbar" {...props} />;
+}

0789d0438264a891353ef10f297a04f9a34c6a1d
askarahodov <devops.bel@yandex.ru>
2026-08-07 12:26:49 +0300

    feat: export replayed data-list primitives for #95

---
+++ app/ui/data-list/index.ts
@@ -0,0 +1,4 @@
+export { DataListPage, type DataListPageProps } from "./DataListPage";
+export { DataListState, type DataListStateKind, type DataListStateProps } from "./DataListState";
+export { DataTable, type DataTableProps } from "./DataTable";
+export { Pagination, type PaginationProps } from "./Pagination";

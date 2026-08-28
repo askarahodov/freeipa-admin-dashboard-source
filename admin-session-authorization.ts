14aec63f51e5bf67d8c34216faf2e0d46e50dfda
askarahodov <devops.bel@yandex.ru>
2026-08-28 16:00:56 +0300

    refactor: use canonical storage integrity contract in admin authorization

--- admin-session-authorization.ts
+++ admin-session-authorization.ts
@@ -1,4 +1,4 @@
-import { STORAGE_INTEGRITY_PATH } from "./storage-integrity-contract.ts";
+import { STORAGE_INTEGRITY_PATH } from "./src/storage/integrity/storage-integrity-contract.ts";
 import { STORAGE_MIGRATION_PREFLIGHT_PATH } from "./storage-migration-preflight-contract.ts";
 import { STORAGE_STATUS_PATH } from "./src/storage/status/storage-status-contract.ts";
 

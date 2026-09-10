2c586a3a92802ab77a5665dde131e69379c540cc
askarahodov <devops.bel@yandex.ru>
2026-09-10 16:33:15 +0300

    fix(settings): defer initial settings load from effect

--- app/settings/general/page.tsx
+++ app/settings/general/page.tsx
@@ -57,7 +57,8 @@ export default function GeneralSettingsPage() {
   }, []);
 
   useEffect(() => {
-    void load();
+    const timer = window.setTimeout(() => void load(), 0);
+    return () => window.clearTimeout(timer);
   }, [load]);
 
   async function createDraft() {

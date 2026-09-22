522b5ae615005922b35c2045ff66e53fae1c4460
askarahodov <devops.bel@yandex.ru>
2026-09-22 17:17:15 +0300

    refactor(settings): delegate local boundary directly to source-safe

--- worker/local-secure-entry.ts
+++ worker/local-secure-entry.ts
@@ -1,4 +1,4 @@
-import secureRuntime from "./settings-input-normalizer-entry";
+import secureRuntime from "./settings-source-safe-entry";
 import { appendAuditEvent, createAuditContext } from "../audit-log";
 import {
   checkLoginRateLimit,
